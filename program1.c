ae6d1da5f3e545f8662046154156843f7eb15bfa
NaveenKumarPN07 <naveenkumarpn50@gmail.com>
2024-12-16 17:06:18 +0530

    program1.c

--- program1.c
+++ program1.c
@@ -1,42 +1,59 @@
 #include<stdio.h>
-int stack[5];
-int top = -1;
-void push(int x){
-    if(top==5-1){
-        printf("stack overflow\n ");//stack over flow
+void push(int *top,int a[],int maxi){
+    if(*top == maxi-1){
+        printf("stack is full!\n");
+        return ;
     }
     else{
-        top++;
-        stack[top] = x;
-        printf("pushed element is %d\n",x);//pushing elements into the stack
+        int value;
+        printf("enter the value:\n");
+        scanf("%d",&value);
+        (*top)++;
+        a[*top] = value;
     }
 }
-void pop (){
-    if(top ==-1 ){
-        printf("under flow\n ");//under flow
+void pop(int a[],int *top){
+    if(*top ==-1){
+        printf("Stack under flow!\n");
+        return ;
     }
-    else{
-        int item ;
-        item = stack[top];
-        top--;
-        printf("poped element is %d\n",item);//poping of top  element
-    }
+    int value = a[*top];
+    (*top)--;
+    printf("poped element is %d\n",value);
 }
-int display(){
-    printf("stack elemnts are :");
-    for(int i= top;i>=0;i--){
-        printf("%d\n",stack[i]);//dispalying elements from top
+void display(int a[],int top){
+    if(top == -1){
+        printf("stack under flow!\n");
+    }
+    for(int i=0;i<=top;i++){
+        printf("%d ",a[top]);
     }
 }
 int main(){
-    push(4);
-    push(2);
-    push(9);
-    push(2);
-    push(6);
-    push(5);
-    pop();
-    display();
-    return 0;
-
+    int maxi = 5;
+    int a[maxi],top = -1,choice;
+    while(1){
+        printf("\nMenu\n");
+        printf("1 . push\n");
+        printf("2 . pop\n");
+        printf("3 . display\n");
+        printf("4 . exit\n");
+        printf("enter your option \n");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1 : 
+                push(&top,a,maxi);
+                break;
+            case 2:
+                pop(a , &top);
+                break;
+            case 3:
+                display(a , top);
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("enter the correct choice!\n");
+        }
+    }
 }
